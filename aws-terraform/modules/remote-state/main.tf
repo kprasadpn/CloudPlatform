4477aa25e47352e28d50ec40afae2d69f440696d
kprasadpn <krishnaprasadp@hotmail.com>
2025-05-09 15:19:14 -0600

    Create main.tf

---
+++ aws-terraform/modules/remote-state/main.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = var.bucket_name
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tf_state_block" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}

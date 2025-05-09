857ff76b3b1401e1fbb384c11bd5c228dda225ff
kprasadpn <krishnaprasadp@hotmail.com>
2025-05-09 14:59:00 -0600

    Update variables.tf

--- aws-terraform/modules/eks/variables.tf
+++ aws-terraform/modules/eks/variables.tf
@@ -1,1 +1,11 @@
+variable "cluster_name" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "cluster_role_arn" {}
+variable "node_role_arn" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "instance_type" {}
 

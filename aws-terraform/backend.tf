6dd5376312275d2bcaf26a538138445d71cbc84f
krishnaprasad <krishnaprasadp@hotmail.com>
2025-05-09 16:34:14 -0600

    Commenting out the backend

--- aws-terraform/backend.tf
+++ aws-terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3" {
+  /*backend "s3" {
     bucket         = "awstfstatekp"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "eks-prod-lock-kp"
-  }
+  }*/
 }

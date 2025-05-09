a86247b3f56d48212a3024cc1ea11abeddbfb75d
kprasadpn <krishnaprasadp@hotmail.com>
2025-05-09 14:48:55 -0600

    Update variables.tf

--- aws-terraform/variables.tf
+++ aws-terraform/variables.tf
@@ -1,1 +1,32 @@
+variable "region" {
+  default = "us-west-2"
+}
 
+variable "cluster_name" {
+  default = "eks-prod"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "desired_capacity" {
+  default = 1
+}
+
+variable "max_size" {
+  default = 2
+}
+
+variable "min_size" {
+  default = 1
+}

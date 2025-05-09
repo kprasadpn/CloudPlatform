e6ef3a4aff31e6e2723d8a910f635833a5633158
kprasadpn <krishnaprasadp@hotmail.com>
2025-05-09 14:57:44 -0600

    Update main.tf

--- aws-terraform/modules/vpc/main.tf
+++ aws-terraform/modules/vpc/main.tf
@@ -1,1 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  availability_zone       = element(["us-west-2a", "us-west-2b"], count.index)
+}
 

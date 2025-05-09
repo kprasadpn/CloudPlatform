6adb1db7fa7f4ae3fe0fa7bfc5fef9249b1af74d
kprasadpn <krishnaprasadp@hotmail.com>
2025-05-09 14:58:46 -0600

    Update main.tf

--- aws-terraform/modules/eks/main.tf
+++ aws-terraform/modules/eks/main.tf
@@ -1,1 +1,23 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+}
+
+resource "aws_eks_node_group" "ng" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-ng"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = [var.instance_type]
+
+  scaling_config {
+    desired_size = var.desired_capacity
+    min_size     = var.min_size
+    max_size     = var.max_size
+  }
+}
 
